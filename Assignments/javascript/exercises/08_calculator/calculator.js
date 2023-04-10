83666978fb3caf50ad1b7965c3b7086f47975081
Mindless-Magnet <muhammedshajahan45@gmail.com>
2023-04-10 18:12:49 +0530

    did calculator

--- Assignments/javascript/exercises/08_calculator/calculator.js
+++ Assignments/javascript/exercises/08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	l = a.length;
+  let s=0;
+  for(let i=0;i<l;i++)
+  {
+    s+=a[i];
+  }
+  return s;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  l = a.length;
+  let s=1;
+  for(let i=0;i<l;i++)
+  {
+    s=s*a[i];
+  }
+  return s;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let r2=1;
+  while(a!=0)
+  {
+    r2 = r2*a;
+    a--;
+  };
+  return r2;
 };
 
 // Do not edit below this line
