c1c4c9f7eccea605b36239bd19b54eebf1ee306e
Mindless-Magnet <muhammedshajahan45@gmail.com>
2023-04-10 19:05:08 +0530

    did till fibonacci

--- Assignments/javascript/exercises/10_fibonacci/fibonacci.js
+++ Assignments/javascript/exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n)
+ {
+    if (n<0)
+    {
+        return "OOPS";
+    };
+    let a =0;let b =1;
+    let tempa =0;
+    let tempb =0;
+    for(let i=0; i<n; i++){
+        tempa = a;
+        tempb = b;
+        a = tempb;
+        b = tempa + tempb;
+    };
+    return a;
 };
 
 // Do not edit below this line
