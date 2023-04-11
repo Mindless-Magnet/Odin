d3b7d3cec0f51da261581e33f432801982880ef3
Mindless-Magnet <muhammedshajahan45@gmail.com>
2023-04-11 09:51:59 +0530

    finished oldest

--- Assignments/javascript/exercises/12_findTheOldest/findTheOldest.js
+++ Assignments/javascript/exercises/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,10 @@
-const findTheOldest = function() {
-
-};
-
+function findTheOldest(people) {
+    return people.reduce(function(prev, current) {
+      var prevAge = prev.yearOfDeath ? prev.yearOfDeath - prev.yearOfBirth : new Date().getFullYear() - prev.yearOfBirth;
+      var currentAge = current.yearOfDeath ? current.yearOfDeath - current.yearOfBirth : new Date().getFullYear() - current.yearOfBirth;
+      return (prevAge > currentAge) ? prev : current;
+    });
+  }
+  
 // Do not edit below this line
 module.exports = findTheOldest;
